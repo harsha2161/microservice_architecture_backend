b9a9ded4b86cfecd199cecb98ca116015ad280b6
R.M.P Harsha <prabhathharsha77@gmail.com>
2026-05-29 19:27:47 +0530

    feat: create order services,controller, DTO and repo

---
+++ order/src/main/java/com/example/order/service/OrderService.java
@@ -0,0 +1,47 @@
+package com.example.order.service;
+
+import com.example.order.dto.OrderDTO;
+import com.example.order.model.Order;
+import com.example.order.repo.OrderRepo;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+
+public class OrderService {
+
+    @Autowired
+    private OrderRepo orderRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public List<OrderDTO> getAllOrders(){
+        List<Order>orderList = orderRepo.findAll();
+        return modelMapper.map(orderList, new TypeToken<List<OrderDTO>>(){}.getType());
+    }
+
+    public OrderDTO createOrder(OrderDTO orderDTO){
+        orderRepo.save(modelMapper.map(orderDTO, Order.class));
+        return orderDTO;
+    }
+
+    public OrderDTO updateOrder(OrderDTO OrderDTO) {
+        orderRepo.save(modelMapper.map(OrderDTO, Order.class));
+        return OrderDTO;
+    }
+
+
+
+
+
+
+
+
+}

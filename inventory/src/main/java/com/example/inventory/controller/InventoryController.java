6e4fd3ae2a810548c938e4dfdacb6aafc2a7c19a
R.M.P Harsha <prabhathharsha77@gmail.com>
2026-06-01 12:38:47 +0530

    feat:improve code

--- inventory/src/main/java/com/example/inventory/controller/InventoryController.java
+++ inventory/src/main/java/com/example/inventory/controller/InventoryController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin
-@RequestMapping(value = "api/v1")
+@RequestMapping(value = "api/v1/inventory")
 
 public class InventoryController {
     @Autowired

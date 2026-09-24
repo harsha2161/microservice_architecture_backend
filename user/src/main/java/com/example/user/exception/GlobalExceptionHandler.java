12c433199ddf7840e0a0dc3a7f36d1774a3d5b28
R.M.P Harsha <prabhathharsha77@gmail.com>
2026-09-24 19:03:28 +0530

    feat: create GlobalExceptionHandler

--- user/src/main/java/com/example/user/exception/GlobalExceptionHandler.java
+++ user/src/main/java/com/example/user/exception/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
-package com.example.user.Exception;
+package com.example.user.exception;
 
-import com.example.user.dto.ResponseDTO;
+import com.example.user.dto.ApiResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,42 +10,42 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler{
 
     @ExceptionHandler(EmailAlreadyExistsException.class)
-    public ResponseEntity<ResponseDTO> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex){
-        ResponseDTO responseDTO = ResponseDTO.builder()
+    public ResponseEntity<ApiResponseDTO> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex){
+        ApiResponseDTO apiResponseDTO = ApiResponseDTO.builder()
                 .message(ex.getMessage())
                 .status(HttpStatus.CONFLICT)
                 .data(null)
                 .build();
-        return new ResponseEntity<>(responseDTO, HttpStatus.CONFLICT);
+        return new ResponseEntity<>(apiResponseDTO, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ResponseDTO> handleResourceNotFoundException(ResourceNotFoundException ex){
-        ResponseDTO responseDTO = ResponseDTO.builder()
+    public ResponseEntity<ApiResponseDTO> handleResourceNotFoundException(ResourceNotFoundException ex){
+        ApiResponseDTO apiResponseDTO = ApiResponseDTO.builder()
                 .message(ex.getMessage())
                 .status(HttpStatus.NOT_FOUND)
                 .data(null)
                 .build();
-        return new ResponseEntity<>(responseDTO, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(apiResponseDTO, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(UserBlockedException.class)
-    public ResponseEntity<ResponseDTO> handleUserBlockedException(UserBlockedException ex){
-        ResponseDTO responseDTO = ResponseDTO.builder()
+    public ResponseEntity<ApiResponseDTO> handleUserBlockedException(UserBlockedException ex){
+        ApiResponseDTO apiResponseDTO = ApiResponseDTO.builder()
                 .message(ex.getMessage())
                 .status(HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS)
                 .data(null)
                 .build();
-        return new ResponseEntity<>(responseDTO, HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS);
+        return new ResponseEntity<>(apiResponseDTO, HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ResponseDTO> handleException(Exception ex){
-        ResponseDTO responseDTO = ResponseDTO.builder()
+    public ResponseEntity<ApiResponseDTO> handleException(Exception ex){
+        ApiResponseDTO apiResponseDTO = ApiResponseDTO.builder()
                 .message("Internal server error" + ex.getMessage())
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .data(null)
                 .build();
-        return new ResponseEntity<>(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(apiResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
